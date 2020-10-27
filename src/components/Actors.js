5ec5e9aaa51f1109f492af96e08bb76010dfb8fb
Elronia <zar.catherine@gmail.com>
2020-10-26 20:05:02 -0400

    Done.

--- src/components/Actors.js
+++ src/components/Actors.js
@@ -5,6 +5,18 @@ const Actors = () => {
   return (
     <div>
       {/*{code here}*/}
+      <h1>Actors Page</h1>
+      {actors.map((actor, id) => (
+          <div key={id}>
+              <h3>Name: {actor.name}</h3>
+              <p>Movies:</p>
+              <ul>
+                  {actor.movies.map((movie, id) => (
+                      <li key={id}>{movie}</li>
+                  ))}
+              </ul>
+          </div>
+      ))}
     </div>
   );
 };
